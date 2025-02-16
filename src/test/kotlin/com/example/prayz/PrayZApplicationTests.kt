6581af0c17d55d9dfc0d927c12c772c0de998fa0
henrique.elias <henrique.elias@ifood.com.br>
2025-02-16 12:11:58 -0300

    Builded my spring structure using: - Kotlin - Gradle - Flyway - JUnit - Courotines - Postgresql

---
+++ src/test/kotlin/com/example/prayz/PrayZApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.prayz
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PrayZApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
